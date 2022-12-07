dc42299064a0595b66796f51cf73a3b322478ac0
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-12-06 21:51:51 -0300

    Add files via upload

--- estatistica_basica.R
+++ estatistica_basica.R
@@ -9,3 +9,14 @@
 ### Se refere ao valor que mais aparece em um conjunto de dados.
 ### Os dados podem ser amodais quando não tem valores repetidos, ou pode ser
 ### bimidal quando tem mais de um valor repetido.
+
+# Média ------------------------------------------------------------------------------------------------------------------------------------
+
+### A média de um conjunto de dados é a soma de todos os valores dividido pelo
+### número total de valores.
+
+# Mediana ----------------------------------------------------------------------------------------------------------------------------------
+
+### Em um conjunto de dados a mediana é o valor central, que divide o conjunto
+### (em ordem crescente ou decrescente) em duas partes iguais, quando não é possível 
+### ter apenas um valor central soma os dois calores do centro e divide por 2. 
